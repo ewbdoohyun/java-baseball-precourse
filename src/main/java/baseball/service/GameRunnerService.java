11496ce9ced945db963d07cabf42d127a4f8170e
doohyun.kim <ewb.doohyun@gmail.com>
2021-09-30 23:16:26 +0900

    feat : 볼 카운트 계산 기능 구현

--- src/main/java/baseball/service/GameRunnerService.java
+++ src/main/java/baseball/service/GameRunnerService.java
@@ -12,4 +12,32 @@ public class GameRunnerService {
         return matchedCnt;
     }
 
+    public int calculateBalls(int target,int input){
+        int matchedCnt = 0;
+
+        matchedCnt += (isBall(target,input/100,3))?1:0;
+        matchedCnt += (isBall(target,input%100/10,2))?1:0;
+        matchedCnt += (isBall(target,input%10,1))?1:0;
+
+        return matchedCnt;
+    }
+
+    /**
+     *
+     * @param target 타겟이 되는 숫자
+     * @param num  1~9까지의 값
+     * @param digit 자릿수
+     * @return
+     */
+    private boolean isBall(int target,int num, int digit){
+        if(target/100 == num && digit !=3){
+            return true;
+        }else if(target%100/10 == num && digit != 2){
+            return true;
+        }else if(target%10 == num && digit != 1){
+            return true;
+        }
+
+        return false;
+    }
 }
--- src/test/java/baseball/service/GameRunnerServiceTest.java
+++ src/test/java/baseball/service/GameRunnerServiceTest.java
@@ -27,4 +27,24 @@ class GameRunnerServiceTest {
         assertEquals(gameRunnerService.calculateStrikes(135,235),2);
         assertEquals(gameRunnerService.calculateStrikes(215,235),2);
     }
+
+    @Test
+    void 볼_테스트() {
+        assertEquals(gameRunnerService.calculateBalls(123,123),0);
+        assertEquals(gameRunnerService.calculateBalls(123,456),0);
+        assertEquals(gameRunnerService.calculateBalls(123,312),3);
+        assertEquals(gameRunnerService.calculateBalls(123,231),3);
+
+        assertEquals(gameRunnerService.calculateBalls(123,514),1);
+        assertEquals(gameRunnerService.calculateBalls(123,541),1);
+        assertEquals(gameRunnerService.calculateBalls(123,356),1);
+        assertEquals(gameRunnerService.calculateBalls(123,536),1);
+        assertEquals(gameRunnerService.calculateBalls(123,672),1);
+        assertEquals(gameRunnerService.calculateBalls(123,276),1);
+
+        assertEquals(gameRunnerService.calculateBalls(123,234),2);
+        assertEquals(gameRunnerService.calculateBalls(123,512),2);
+        assertEquals(gameRunnerService.calculateBalls(123,351),2);
+    }
+
 }
