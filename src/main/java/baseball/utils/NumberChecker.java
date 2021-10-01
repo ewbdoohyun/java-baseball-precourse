973b53ca7102261e96b5dce5126b1917f7ec75bf
doohyun.kim <ewb.doohyun@gmail.com>
2021-10-01 18:46:32 +0900

    refactor : 번호 유효성 체크 Util의 조건 추가

--- src/main/java/baseball/utils/NumberChecker.java
+++ src/main/java/baseball/utils/NumberChecker.java
@@ -2,13 +2,17 @@ package baseball.utils;
 
 public class NumberChecker {
 
-    private NumberChecker(){ }
+    private NumberChecker() {
+    }
 
     public static boolean isValidBaseBallNum(int num) {
+        if (num >= 1000 || num < 123) {
+            return false;
+        }
         int hundredsNum = num / 100;
         int tensNum = num % 100 / 10;
         int unitsNum = num % 10;
-        if(hundredsNum == 0 || tensNum == 0 || unitsNum == 0){
+        if (hundredsNum == 0 || tensNum == 0 || unitsNum == 0) {
             return false;
         }
         return hundredsNum != tensNum && tensNum != unitsNum && hundredsNum != unitsNum;
