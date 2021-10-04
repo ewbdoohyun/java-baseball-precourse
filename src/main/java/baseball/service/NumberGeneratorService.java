ce8ae1019baa7dc7c16bb6d091976b0a83552377
doohyun.kim <ewb.doohyun@gmail.com>
2021-10-04 11:19:35 +0900

    refactor : testCode 명세에 따라 번호를 각각 가져오도록 수정

--- src/main/java/baseball/service/NumberGeneratorService.java
+++ src/main/java/baseball/service/NumberGeneratorService.java
@@ -7,8 +7,11 @@ public class NumberGeneratorService {
 
     public int getNewNumber() {
         int baseballNum;
-        while(!NumberChecker.isValidBaseballNum(baseballNum= Randoms.pickNumberInRange(123,987)));
+        while(!NumberChecker.isValidBaseballNum(baseballNum= getNumber()));
         return baseballNum;
+    }
+    private int getNumber(){
+        return Randoms.pickNumberInRange(1,9)*100+ Randoms.pickNumberInRange(1,9)*10+ Randoms.pickNumberInRange(1,9);
     }
 
 }
