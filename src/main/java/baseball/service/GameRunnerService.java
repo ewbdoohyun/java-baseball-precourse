16810152d34f4d214cc3272db874199c8b308468
doohyun.kim <ewb.doohyun@gmail.com>
2021-10-04 11:59:09 +0900

    feat : 게임 진행 기능 구현.

--- src/main/java/baseball/service/GameRunnerService.java
+++ src/main/java/baseball/service/GameRunnerService.java
@@ -1,43 +1,47 @@
 package baseball.service;
 
+import baseball.utils.NumberChecker;
+import nextstep.utils.Console;
+
 public class GameRunnerService {
 
-    public int calculateStrikes(int target, int input){
-        int matchedCnt = 0;
+    public static final int WRONG_INPUT_NUM = 0;
 
-        matchedCnt += (target / 100 == input / 100) ? 1 : 0;
-        matchedCnt += (target % 100 / 10 == input % 100 / 10) ? 1 : 0;
-        matchedCnt += (target % 10 == input % 10) ? 1 : 0;
+    private final NumberGeneratorService numberGeneratorService;
+    private final PrintTurnResultService printTurnResultService;
 
-        return matchedCnt;
+    public GameRunnerService() {
+        numberGeneratorService = new NumberGeneratorService();
+        printTurnResultService = new PrintTurnResultService();
     }
 
-    public int calculateBalls(int target,int input){
-        int matchedCnt = 0;
-
-        matchedCnt += (isBall(target,input/100,3))?1:0;
-        matchedCnt += (isBall(target,input%100/10,2))?1:0;
-        matchedCnt += (isBall(target,input%10,1))?1:0;
-
-        return matchedCnt;
+    public void startGame() {
+        int baseballNum = numberGeneratorService.getNewNumber();
+        int strikes,balls;
+        do {
+            int inputNum = readNumber();
+            strikes = NumberChecker.calculateStrikes(baseballNum, inputNum);
+            balls = NumberChecker.calculateBalls(baseballNum, inputNum);
+            printTurnResultService.printResult(inputNum, strikes, balls);
+        } while (strikes != 3);
+        printTurnResultService.printGameEndResult();
     }
 
-    /**
-     *
-     * @param target 타겟이 되는 숫자
-     * @param num  1~9까지의 값
-     * @param digit 자릿수
-     * @return
-     */
-    private boolean isBall(int target,int num, int digit){
-        if(target/100 == num && digit !=3){
-            return true;
-        }else if(target%100/10 == num && digit != 2){
-            return true;
-        }else if(target%10 == num && digit != 1){
-            return true;
+    private int readNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        try {
+            int num = Integer.parseInt(Console.readLine());
+            return validateBaseBallNumber(num);
+        } catch (Exception e ) {
+            return WRONG_INPUT_NUM;
         }
-
-        return false;
     }
+
+    private int validateBaseBallNumber(int num) throws Exception {
+        if (!NumberChecker.isValidBaseballNum(num)) {
+            throw new Exception("번호 오류 ");
+        }
+        return num;
+    }
+
 }
